583de497133c0e6de30cd0f2f379793cedfb5b20
KeslieBox <kesliebox@gmail.com>
2021-05-03 18:26:58 -0500

    herb model, migration and seeds

---
+++ app/models/herb.rb
@@ -0,0 +1,5 @@
+class Herb < ApplicationRecord
+    # has_many :properties, through: :qualities 
+    # has_many :flavors, through: :qualities 
+    # has_many :energetics, through: :qualities 
+end
